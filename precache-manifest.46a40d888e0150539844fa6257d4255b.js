26959286d1a5cb3ae193f82f9911e5a6f33eefbb
Adnan <adnan_ghafoor94@hotmail.com>
2020-08-30 20:22:23 +0100

    Updates

---
+++ precache-manifest.46a40d888e0150539844fa6257d4255b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db38d4b240a7a84e8c97255db2e2762d",
+    "url": "/portfolio-v3/index.html"
+  },
+  {
+    "revision": "e2228b551cb36ae13566",
+    "url": "/portfolio-v3/static/css/2.58c4053f.chunk.css"
+  },
+  {
+    "revision": "7b672acc408f31d320df",
+    "url": "/portfolio-v3/static/css/main.01587160.chunk.css"
+  },
+  {
+    "revision": "e2228b551cb36ae13566",
+    "url": "/portfolio-v3/static/js/2.567b05e8.chunk.js"
+  },
+  {
+    "revision": "c7302c59c8c8cd32994a469803bd79b9",
+    "url": "/portfolio-v3/static/js/2.567b05e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b672acc408f31d320df",
+    "url": "/portfolio-v3/static/js/main.81082afc.chunk.js"
+  },
+  {
+    "revision": "aad0a4237f81d92b0a42",
+    "url": "/portfolio-v3/static/js/runtime-main.cdb215b3.js"
+  },
+  {
+    "revision": "ee3a7d7faf815142e50fef37b12f6b6d",
+    "url": "/portfolio-v3/static/media/Adnan_GhafoorCV.ee3a7d7f.pdf"
+  }
+]);
