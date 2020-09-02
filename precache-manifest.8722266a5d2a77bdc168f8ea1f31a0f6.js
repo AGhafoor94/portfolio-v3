35bf804273532379a005be6533e88ae244ca69e9
Adnan <adnan_ghafoor94@hotmail.com>
2020-09-02 17:39:08 +0100

    Updates

---
+++ precache-manifest.8722266a5d2a77bdc168f8ea1f31a0f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7c6b735f83cbc6acc9d05a3fbe6b3d",
+    "url": "/portfolio-v3/index.html"
+  },
+  {
+    "revision": "e2228b551cb36ae13566",
+    "url": "/portfolio-v3/static/css/2.58c4053f.chunk.css"
+  },
+  {
+    "revision": "ddfbe105131759d7c829",
+    "url": "/portfolio-v3/static/css/main.01587160.chunk.css"
+  },
+  {
+    "revision": "e2228b551cb36ae13566",
+    "url": "/portfolio-v3/static/js/2.567b05e8.chunk.js"
+  },
+  {
+    "revision": "c7302c59c8c8cd32994a469803bd79b9",
+    "url": "/portfolio-v3/static/js/2.567b05e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddfbe105131759d7c829",
+    "url": "/portfolio-v3/static/js/main.39b8647a.chunk.js"
+  },
+  {
+    "revision": "aad0a4237f81d92b0a42",
+    "url": "/portfolio-v3/static/js/runtime-main.cdb215b3.js"
+  },
+  {
+    "revision": "ee3a7d7faf815142e50fef37b12f6b6d",
+    "url": "/portfolio-v3/static/media/Adnan_GhafoorCV.ee3a7d7f.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.382ebde4b2c5a88cceefcba2b56a4941.js"
+  "/portfolio-v3/precache-manifest.8722266a5d2a77bdc168f8ea1f31a0f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-v3/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
